35bc245baf6a03413ab36a68bcc944f9c3128d19
mrtubelight <87597101+mrtubelight@users.noreply.github.com>
2021-07-22 02:17:34 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -115,7 +115,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirror Bot',
+            'description': 'Uploaded using Mirror Tube',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -169,7 +169,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirror Bot',
+            'description': 'Uploaded by Mirror Tube',
             'mimeType': mime_type,
         }
         try:
